898592a3522779235d8145a06afda0e631c9ff15
Bentom7 <benzzoy07@gmail.com>
2019-01-18 14:01:22 +0800

    Atay mao diay human nako sir

--- app/src/main/java/com/tomas/formvalidation/SignupActivity.java
+++ app/src/main/java/com/tomas/formvalidation/SignupActivity.java
@@ -4,14 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
-    EditText etUsername, etPassword, etConfirmpassword, etFullname;
-    String username, password, Confirmpaswword, Fullname;
+    EditText etUsername, etPassword, etConfirmPassword, etEnterFullname;
+    String username, password, confirmpassword, enterfullname;
     int formsuccess;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +22,8 @@ public class SignupActivity extends AppCompatActivity {
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
-        etConfirmpassword = findViewById(R.id.etConfirmpassword);
-        etFullname = findViewById(R.id.etFullname);
+        etConfirmPassword = findViewById(R.id.etConfirmpassword);
+        etEnterFullname = findViewById(R.id.etFullname);
 
     }
 
@@ -33,42 +35,47 @@ public class SignupActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-       switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.btnSave:
-                formsuccess = 4;
+
+                formsuccess = 5;
 
                 username = etUsername.getText().toString();
                 password = etPassword.getText().toString();
-                Confirmpaswword = etConfirmpassword.getText().toString();
-                Fullname = etFullname.getText().toString();
+                confirmpassword = etConfirmPassword.getText().toString();
+                enterfullname = etEnterFullname.getText().toString();
 
-                if (username.equals("")){
+                if (username.equals("")) {
                     etUsername.setError("This field is required");
                     formsuccess--;
                 }
-
-                if (password.equals("")){
+                if (password.equals("")) {
                     etPassword.setError("This field is required");
                     formsuccess--;
                 }
-
-                if (Confirmpaswword.equals("")){
-                    etConfirmpassword.setError("This field is required");
+                if (confirmpassword.equals("")) {
+                    etConfirmPassword.setError("This field is required");
                     formsuccess--;
                 }
 
-                if (Fullname.equals("")){
-                    etFullname.setError("This field is required");
+                if (!confirmpassword.equals(password)){
+                    etConfirmPassword.setError("Password Mismatch");
+                    formsuccess --;
+                }
+
+                if (enterfullname.equals("")) {
+                    etEnterFullname.setError("This field is required");
                     formsuccess--;
                 }
 
-
+                Toast.makeText(this, "Validate Form Test", Toast.LENGTH_SHORT).show();
                 break;
-           case R.id.btnCancel:
-               this.finish();
+            case R.id.btnCancel:
+                this.finish();
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
-}
 
+
+}
